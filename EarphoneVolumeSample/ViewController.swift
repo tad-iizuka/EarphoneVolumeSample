d5d742a26ace0ddf68b4e6d9b487b5b06e5804da
tad-iizuka <tad.iizuka@gmail.com>
2016-09-22 21:31:33 +0900

    Initial Commit

---
+++ EarphoneVolumeSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarphoneVolumeSample
+//
+//  Created by Tadashi on 2016/09/22.
+//  Copyright © 2016 T@d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
